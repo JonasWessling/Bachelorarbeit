7d28114fdb6398c486f9d2cce727cd5070525fe6
Jonas <jonas.wessling,@ewetel.net>
2026-08-07 17:06:20 +0200

    Install and setup redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "bulma/css/bulma.css";
 import "./styles/main.scss";
 import App from "./App.jsx";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
