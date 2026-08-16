11ee610ea3f5858077ec2d1097d16c1e1ef805d3
Jonas <jonas.wessling,@ewetel.net>
2026-08-16 15:49:13 +0200

    fix how images are loaded

--- frontend/src/components/FoodRules.jsx
+++ frontend/src/components/FoodRules.jsx
@@ -1,16 +1,6 @@
-import Allowed_de from "../assets/images/Allowed_de.png";
-import Forbidden_de from "../assets/images/Forbidden_de.png";
-
-import Allowed_en from "../assets/images/Allowed_en.png";
-import Forbidden_en from "../assets/images/Forbidden_en.png";
-
-import Allowed_de_dark from "../assets/images/Allowed_de_dark.png";
-import Forbidden_de_dark from "../assets/images/Forbidden_de_dark.png";
-
-import Allowed_en_dark from "../assets/images/Allowed_en_dark.png";
-import Forbidden_en_dark from "../assets/images/Forbidden_en_dark.png";
 import { useTranslation } from "react-i18next";
-import { useTheme } from "../provider/ThemeProvider.jsx";
+import { Themes, useTheme } from "../provider/ThemeProvider.jsx";
+import { useEffect, useState } from "react";
 
 const FoodRules = () => {
   const { i18n, t } = useTranslation();
@@ -18,18 +8,30 @@ const FoodRules = () => {
 
   const lang = i18n.language.startsWith("de") ? "de" : "en";
 
-  const imageMap = {
-    light: {
-      de: { allowed: Allowed_de, forbidden: Forbidden_de },
-      en: { allowed: Allowed_en, forbidden: Forbidden_en },
-    },
-    dark: {
-      de: { allowed: Allowed_de_dark, forbidden: Forbidden_de_dark },
-      en: { allowed: Allowed_en_dark, forbidden: Forbidden_en_dark },
-    },
+  const loadImage = async (theme, lang, type) => {
+    if (theme === Themes.HighContrast) {
+      theme = Themes.DarkMode;
+    }
+    try {
+      const img = await import(
+        `../assets/images/foodRules/${theme}/${lang}/${type}.png`
+      );
+      return img.default;
+    } catch {
+      const fallback = await import(
+        `../assets/images/foodRules/light/de/${type}.png`
+      );
+      return fallback.default;
+    }
   };
 
-  const current = imageMap[theme]?.[lang] ?? imageMap.light.de;
+  const [allowed, setAllowed] = useState(null);
+  const [forbidden, setForbidden] = useState(null);
+
+  useEffect(() => {
+    loadImage(theme, lang, "allowed").then(setAllowed);
+    loadImage(theme, lang, "forbidden").then(setForbidden);
+  }, [theme, lang]);
 
   return (
     <div>
@@ -37,10 +39,11 @@ const FoodRules = () => {
         <h2 className="title">{t("foodRules")}</h2>
       </div>
       <div style={{ width: "350px" }} className="is-flex is-gap-2 mt-5">
-        <img src={current.allowed} alt={t("foodRulesAllowedAlt")} />
-        <img src={current.forbidden} alt={t("foodRulesForbiddenAlt")} />
+        <img src={allowed} alt={t("foodRulesAllowedAlt")} />
+        <img src={forbidden} alt={t("foodRulesForbiddenAlt")} />
       </div>
     </div>
   );
 };
+
 export default FoodRules;
