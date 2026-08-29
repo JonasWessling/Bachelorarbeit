14d51c10595b0b1ea50aae3df80d37e668fcdbd8
Jonas <jonas.wessling,@ewetel.net>
2026-08-29 18:34:54 +0200

    remove unused stuff

--- backend/src/main/java/com/example/backend/booksearch/service/BookSearchService.java
+++ backend/src/main/java/com/example/backend/booksearch/service/BookSearchService.java
@@ -11,7 +11,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 public class BookSearchService {
